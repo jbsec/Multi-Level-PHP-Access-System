3a164058e42945cd29cbe47f7cd6d9b4a7cdcb39
James <45885505+JNBarr@users.noreply.github.com>
2019-06-15 17:57:59 +0100

    Add files via upload

---
+++ home.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<h1> Welcome to the Staff Portal </h1>
+
+<form method="post" action="do_logout.php">
+<input type="submit" name="logout" value="LOGOUT" />	
+</form>
+</body>
+</html>
---
+++ home_admin.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+
+<h1> Welcome Manager. </h1>
+
+</body>
+</html>
